8d481fb84b003823073067e45b669c352a28c7ec
tiagospp55 <tiagosilvapereira55@gmail.com>
2022-03-28 03:09:46 +0100

    SPA feito e falta arranjar o menu e o Choose file que nao muda

---
+++ Frontend/pages/profile.php
@@ -0,0 +1,15 @@
+<?php
+  session_start();
+  $_SESSION["last_page"] = "profile.php";
+?>
+
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+      <div class="col-sm-6">
+        <h1>Profile</h1>
+      </div>        
+    </div>
+  </div><!-- /.container-fluid -->
+</section>
