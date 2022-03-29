b7d0c217fc2c1c9b3b6d4ea8bb431e5bc2480322
manumir <49996755+manumir@users.noreply.github.com>
2022-03-29 21:18:08 +0100

    Add files via upload

---
+++ Analise_de_dados/paginas/graph_blood_pressure.js
@@ -0,0 +1,78 @@
+let httpRequest = new XMLHttpRequest();
+// aqui têm de passar o nome correto do ficheiro json
+
+httpRequest.open("GET", "teste_blood_presure_processado.json", true);
+httpRequest.send();
+httpRequest.addEventListener("readystatechange", function() {
+    if (this.readyState === this.DONE) {
+      	// when the request has completed
+        let json_file = JSON.parse(this.response);
+        //console.log(Object.keys(json_file))
+
+        let labels = []
+        let pulse= []
+        let dia_max= []
+        let dia_min= []
+        let sys_max= []
+        let sys_min= []
+        for (let key of Object.keys(json_file)) {
+          labels.push(json_file[key]["day"].substring(0,11))
+          pulse.push(json_file[key]["Pulse_mean"])
+          dia_min.push(json_file[key]["DIA_min"])
+          dia_max.push(json_file[key]["DIA_max"])
+          sys_min.push(json_file[key]["SYS_min"])
+          sys_max.push(json_file[key]["SYS_max"])
+        }
+	var joined_dia=[];
+	for (let i = 0; i < dia_min.length; i++) {
+		var temp=[];
+  	temp.push(dia_min[i]);
+  	temp.push(dia_max[i]);
+		joined_dia.push(temp);
+	}
+	var joined_sys=[];
+	for (let i = 0; i < sys_min.length; i++) {
+		var temp=[];
+  	temp.push(sys_min[i]);
+  	temp.push(sys_max[i]);
+		joined_sys.push(temp);
+	}
+
+	const ctx = document.getElementById('line-chart');
+		const data = {
+					labels:labels,
+					datasets: [{
+						type:'bar',
+						label: 'DIA',
+						data:joined_dia,
+						backgroundColor: 'rgba(52, 186, 235)',
+						barPercentage:0.8,
+					},
+					{
+						type:'line',
+						label: 'Pulse mean',
+						data:pulse,
+						borderColor: '#34eb5f',
+					},
+					{
+						type:'bar',
+						label: 'SYS',
+						data:joined_sys,
+						backgroundColor: '#eb3440',
+						barPercentage:0.8,
+					}]
+		};
+		const config = {
+			type: 'bar',
+			data:data,
+			options: {
+				scales: {
+					x: {
+						stacked: true,
+					},
+    		}
+			}
+		};
+		const myChart = new Chart(ctx,config);
+	}
+});
