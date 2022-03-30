e5827c59d0764d7162b75925f8c2542a2a0be7e0
manteigas12 <euseilacrlh@gmail.com>
2022-03-30 02:03:48 +0100

    yuu

--- Frontend/TemPeso.php
+++ Frontend/TemPeso.php
@@ -1,5 +1,7 @@
 <?php 
-	echo "CON";
-
-
- ?>
+	if ($_SESSION["peso"]=="") {
+			echo "<input type='text' name='peso' style='width:80%;'>";
+	}else{
+		echo "<a class='float-right'>".$_SESSION["peso"]."</a>";
+	}
+?>
